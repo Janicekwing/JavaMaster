b5359d4fc51396696d64e856e77e5e5ab8eb3ce9
Janice He <janicekwhe@gmail.com>
2018-10-27 22:10:36 -0400

    Initial commit

---
+++ Account.java
@@ -0,0 +1,63 @@
+public class Account{
+    
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmailAddress;
+    private String customerPhoneNumber;
+
+    public Account(){
+        
+    }
+
+    public void deposit(int amount){
+        this.balance += amount;
+        System.out.printf("You now have " + this.balance + " in your account \n");
+    }
+
+    public void withdraw(int amount){
+        if (this.balance - amount > 0){
+            this.balance -= amount;
+            System.out.printf("You now have " + this.balance + " in your account \n");
+        }
+        else {
+            System.out.println("Insufficient funds \n");
+        }
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public String getNumber() {
+        return number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerPhoneNumber() {
+        return customerPhoneNumber;
+    }
+    public void setCustomerPhoneNumber(String customerPhoneNumber) {
+        this.customerPhoneNumber = customerPhoneNumber;
+    }
+
+    public String getCustomerEmailAddress() {
+        return customerEmailAddress;
+    }
+    public void setCustomerEmailAddress(String customerEmailAddress) {
+        this.customerEmailAddress = customerEmailAddress;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        Account janicesAccount = new Account();
+        janicesAccount.deposit(900);
+        janicesAccount.withdraw(80);
+    }
+
+}
